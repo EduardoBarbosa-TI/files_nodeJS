d4b66f6d74b8715e593fca948cf8d6c602c59a7c
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-07-24 14:38:59 -0300

    feat: working with fs files

--- intro_course/app.js
+++ intro_course/app.js
@@ -1,13 +1,26 @@
 const fs = require('fs')
 
+//Ler arquivos
 fs.readFile('calc.js','utf8', function(err, data){
     console.log(data)
 })
 
+//Atualizar arquivos
 fs.writeFile('calc1.js','console.log("done")',function(err){
     console.log("Data Saved")
 })
+fs.appendFile('calculo.js', ' This is my text.', function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+});
 
+//Deletar arquivos
 fs.unlink('calc1.js',function(err){
     console.log('Deleted')
+})
+
+//Renomear arquivos
+fs.rename('calc.js','calculos.js', function(err){
+    if(err) throw err
+    console.log("File rename!")
 })
